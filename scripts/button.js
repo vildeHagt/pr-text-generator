a4b9a19da89e040b093904de99f4ee9e46b202f5
Vilde Hagtvedt <vilde.hagtvedt@spv.no>
2025-05-22 11:18:04 +0200

    Button is added

--- scripts/button.js
+++ scripts/button.js
@@ -1,4 +1,12 @@
-const modal = document.getElementById("review-changes-modal");
-if (modal) {
-  console.log("Modal found");
+const actionBar = document.getElementsByClassName("ActionBar")[0];
+
+if (actionBar) {
+  console.log("ActionBar found");
+
+  const button = document.createElement("button");
+  button.style.backgroundColor = "transparent";
+  button.style.border = "none";
+  button.textContent = "✨";
+
+  actionBar.appendChild(button);
 }
